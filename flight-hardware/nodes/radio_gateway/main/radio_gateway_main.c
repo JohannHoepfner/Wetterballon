69a3b51b19568b940bb7f449c1dfa646326e8b7c
Paul <contact@paulmaier.online>
2026-09-06 12:07:44 +0200

    remove sim_modem from radio gateway

--- flight-hardware/nodes/radio_gateway/main/radio_gateway_main.c
+++ flight-hardware/nodes/radio_gateway/main/radio_gateway_main.c
@@ -18,7 +18,6 @@
 #include "log_store.h"
 #include "neo_m8.h"
 #include "nvs_flash.h"
-#include "sim_modem.h"
 #include "sdkconfig.h"
 #include "sensor_task.h"
 
@@ -56,49 +55,6 @@ static void on_databus_log(struct databus_message *msg)
     aggregate_message("LOG", msg->send_time, msg->log.message, sizeof(msg->log.message));
 }
 
-static void send_task(void *arg)
-{
-    (void)arg;
-
-    int backoff_sec = 5;
-    while (sim_modem.init() != ESP_OK) {
-        ESP_LOGE(TAG, "sim_modem.init() failed, retrying in %d s", backoff_sec);
-        vTaskDelay(pdMS_TO_TICKS(backoff_sec * 1000));
-        backoff_sec = backoff_sec < 300 ? backoff_sec * 2 : 300;
-    }
-    ESP_LOGI(TAG, "Modem ready");
-
-    static char body[AGG_BUF_SIZE];
-    while (1) {
-        size_t snapshot_len;
-
-        xSemaphoreTake(s_agg_mutex, portMAX_DELAY);
-        snapshot_len = s_agg_len;
-        memcpy(body, s_agg_buf, snapshot_len);
-        xSemaphoreGive(s_agg_mutex);
-
-        size_t body_len = snapshot_len;
-        if (snapshot_len == 0) {
-            body_len = snprintf(body, sizeof(body), "(no intracom messages since last send)\r\n");
-        }
-
-        esp_err_t err = sim_modem.send(body, body_len);
-        if (err != ESP_OK) {
-            ESP_LOGE(TAG, "Sending email failed, will retry next cycle (messages stay queued)");
-        } else if (snapshot_len > 0) {
-            xSemaphoreTake(s_agg_mutex, portMAX_DELAY);
-            if (s_agg_len >= snapshot_len) {
-                size_t remaining = s_agg_len - snapshot_len;
-                memmove(s_agg_buf, s_agg_buf + snapshot_len, remaining);
-                s_agg_len = remaining;
-            }
-            xSemaphoreGive(s_agg_mutex);
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(CONFIG_MODEM_SEND_INTERVAL_SEC * 1000));
-    }
-}
-
 void app_main(void)
 {
     s_agg_mutex = xSemaphoreCreateMutex();
@@ -118,10 +74,10 @@ void app_main(void)
     ESP_ERROR_CHECK(intracom->register_recv_callback(DATABUS_MSG_TYPE_LOG, on_databus_log));
     ESP_ERROR_CHECK(store->init());
 
-    BaseType_t task_created = xTaskCreate(send_task, "modem_send", 4096, NULL, 5, NULL);
-    if (task_created != pdPASS) {
-        ESP_LOGE(TAG, "Failed to create modem send task");
-    }
+    // BaseType_t task_created = xTaskCreate(send_task, "sim_modem_send", 4096, NULL, 5, NULL);
+    // if (task_created != pdPASS) {
+    //     ESP_LOGE(TAG, "Failed to create modem send task");
+    // }
 
     Sensor *sensor_gps = &neo_m8;
 
